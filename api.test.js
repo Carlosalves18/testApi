a32392ee3d520b0b5e8024cbd4fd04fad0b0a81e
Carlos Eduardo <138811560+Carlosalves18@users.noreply.github.com>
2024-03-06 18:29:12 -0300

    Add files via upload

---
+++ api.test.js
@@ -0,0 +1,25 @@
+// const axios = require('axios')
+
+//  async function Api(){
+//     const response = await axios.get('https://api.chucknorris.io/jokes/random')
+    
+//     return response.data
+// }
+// test('Verifica dados obtidos da API', async ()=>{
+//     const dados = await Api()
+//     expect(dados).toBeDefined();
+// })
+const axios = require('axios');
+
+// Função que faz a chamada à API
+async function getApiData() {
+  try {
+    const response = await axios.get(' https://rickandmortyapi.com/api');
+    return response.data; 
+  } catch (error) {
+    console.error('Erro ao obter dados da API:', error);
+    throw error;
+  }
+}
+
+module.exports = { getApiData };
